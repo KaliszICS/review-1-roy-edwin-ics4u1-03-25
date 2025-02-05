af7790e53f2760e58a3225f9d8dda674cba99dca
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-02-04 23:23:20 -0500

    Update Review1.java

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,29 @@
 public class Review1 {
-
-	public static void q1() {
-		//Write question 1 code here
+	public static void q1() { System.out.println("There once was a man from St. Ives.");
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static void q2() { 
+		x = 5;
+		y = 9; 
+System.out.println( x / y );
+System.out.println( x % y );
 	}
 
-	public static void q3() {
+	public static void q3() { System.out.println("Hello" "Hello again");
 		//Write question 3 code here
 	}
 
-	public static void q4() {
+	public static void q4() { 
+boolean yes = true
+	System.out.println( boolean yes); 
 		//Write question 4 code here
 	}
 
-	public static void q5() {
-		//Write question 5 code here
+	public static void q5() { 
+		ok = "Hello"; 
+ 		no = "World";
+		System.out.println( ok ); 
+		System.out.println( no );
 	}
 
 }
